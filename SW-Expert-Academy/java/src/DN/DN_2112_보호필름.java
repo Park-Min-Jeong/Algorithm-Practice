793b489d5824a589336d180989fa6f57fadd9571
Park-Min-Jeong <minwjd1028@naver.com>
2024-04-03 12:57:15 +0900

    solved: 240403 SW-Expert-Academy DN_2112_보호필름.java

--- SW-Expert-Academy/java/src/DN/DN_2112_보호필름.java
+++ SW-Expert-Academy/java/src/DN/DN_2112_보호필름.java
@@ -11,31 +11,55 @@ public class DN_2112_보호필름 {
     static boolean[] visited;
     static int answer;
 
+    static int[] a0;
+    static int[] a1;
 
     static boolean performanceTest(int[][] filmCopy) {
         boolean answer = true;
 
         for (int x = 0; x < w; x++) {
-            int maxCount = 0;
-            int check = -1;
-            int tempCount = 0;
-            for (int y = 0; y < d; y++) {
-                if (filmCopy[y][x] != check) {
-                    if (tempCount > maxCount) maxCount = tempCount;
-                    tempCount = 0;
-                    check = filmCopy[y][x];
+            int count = 1;
+            for (int y = 1; y < d; y++) {
+                if (filmCopy[y][x] == filmCopy[y - 1][x]) {
+                    count++;
                 }
-                tempCount++;
+                else {
+                    count = 1;
+                }
+                if (count == k) {
+                    break;
+                }
             }
-            if (tempCount > maxCount) maxCount = tempCount;
-
-            if (maxCount < k) {
+            if (count < k) {
                 answer = false;
                 break;
             }
         }
         
         return answer;
+    }
+
+    static int[][] addMedicine(int[][] filmCopy, int yIndex, int[] a) {
+        filmCopy[yIndex] = a;
+        return filmCopy;
+    }
+
+    static void dfs(int count, int yIndex, int[][] filmCopy) {
+        if (performanceTest(filmCopy)) {
+            answer = Math.min(answer, count);
+            return;
+        }
+
+        if (yIndex == d) {
+            return;
+        }
+
+        int[] temp = new int[w];
+        System.arraycopy(filmCopy[yIndex], 0, temp, 0, w);
+
+        dfs(count + 1, yIndex + 1, addMedicine(filmCopy, yIndex, a0));
+        dfs(count + 1, yIndex + 1, addMedicine(filmCopy, yIndex, a1));
+        dfs(count, yIndex + 1, addMedicine(filmCopy, yIndex, temp));
     }
 
     public static void main(String[] args) {
@@ -49,9 +73,14 @@ public class DN_2112_보호필름 {
             d = sc.nextInt();
             w = sc.nextInt();
             k = sc.nextInt();
+            answer = Integer.MAX_VALUE;
 
             film = new int[d][w];
             visited = new boolean[w];
+
+            a0 = new int[w];
+            a1 = new int[w];
+            Arrays.fill(a1, 1);
 
             for (int y = 0; y < d; y++) {
                 for (int x = 0; x < w; x++) {
@@ -59,7 +88,9 @@ public class DN_2112_보호필름 {
                 }
             }
 
-            System.out.printf("#%d %s\n", t);
+            dfs(0, 0, film.clone());
+
+            System.out.printf("#%d %d\n", t, answer == Integer.MAX_VALUE ? -1: answer);
 
         }
 
